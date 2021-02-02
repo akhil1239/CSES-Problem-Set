a184622f7c261c1df32091884e6e6d933836b0ca
akhil1239 <78419720+akhil1239@users.noreply.github.com>
2021-02-02 19:12:19 +0530

    Create Weird Algorithm.cpp

---
+++ Introductory Problems/Weird Algorithm.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	if(n>0){
+	cout<<n<<" ";
+	while(n!=1){
+		if(n%2==0)
+			n=n/2;
+		else
+			n=n*3+1;
+		cout<<n<<" ";
+		}
+	}
+}
